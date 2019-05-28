ec0feb089a4c844d05358d50157b2700ff471ed3
home.yxf.pc <yangxufeng2007@home.com>
2019-05-28 21:44:38 +0800

    feat(module): 1.增加scroll和swiper组件

---
+++ src/components/scroll/scroll.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit } from 'vue-property-decorator';
+
+@Component
+export default class Scroll extends Vue {
+  private state: number = 0; // 0 = 滚动中 1 = 滚动加载中 -1 = 滚动结束
+
+  protected mounted() {
+    console.log('mounted');
+  }
+
+  @Emit('load')
+  private loading() {
+    console.log('load');
+  }
+}
+</script>
---
+++ src/components/swiper/swiper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit } from 'vue-property-decorator';
+
+@Component
+export default class Swiper extends Vue {
+  protected mounted() {
+    console.log('mounted');
+  }
+
+  @Emit('load')
+  private loading() {
+    console.log('load');
+  }
+}
+</script>
